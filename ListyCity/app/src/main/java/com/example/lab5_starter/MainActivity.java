4df618347c41681d5f69ba04bfe7d2d93b14922f
Yousef Moussa <129340216+YousefMoussa@users.noreply.github.com>
2025-10-15 17:24:59 -0600

    Initial commit

---
+++ ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.lab5_starter;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CityDialogFragment.CityDialogListener {
+
+    private Button addCityButton;
+    private Button deleteCityButton;
+    private ListView cityListView;
+
+    private ArrayList<City> cityArrayList;
+    private ArrayAdapter<City> cityArrayAdapter;
+
+    private FirebaseFirestore db;
+    private CollectionReference citiesRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        db = FirebaseFirestore.getInstance();
+        citiesRef = db.collection("cities");
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set views
+        addCityButton = findViewById(R.id.buttonAddCity);
+        cityListView = findViewById(R.id.listviewCities);
+        deleteCityButton = findViewById(R.id.del_btn);
+
+        // create city array
+        cityArrayList = new ArrayList<>();
+        cityArrayAdapter = new CityArrayAdapter(this, cityArrayList);
+        cityListView.setAdapter(cityArrayAdapter);
+
+//        addDummyData();
+
+        citiesRef.addSnapshotListener((QuerySnapshot value, FirebaseFirestoreException error) -> {
+            if (error != null){
+                Log.e("Firestore", error.toString());
+            }
+            if (value != null && !value.isEmpty()){
+                cityArrayList.clear();
+                for (QueryDocumentSnapshot snapshot : value){
+                    String name = snapshot.getString("name");
+                    String province = snapshot.getString( "province");
+
+                    cityArrayList.add(new City(name, province));
+                }
+                cityArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // set listeners
+        addCityButton.setOnClickListener(view -> {
+            CityDialogFragment cityDialogFragment = new CityDialogFragment();
+            cityDialogFragment.show(getSupportFragmentManager(),"Add City");
+        });
+
+        deleteCityButton.setOnClickListener(view -> {
+            if (cityArrayList.isEmpty()) {
+                new android.app.AlertDialog.Builder(MainActivity.this)
+                        .setTitle("No Cities")
+                        .setMessage("There are no cities to remove.")
+                        .setPositiveButton("OK", (dialog, listener) -> dialog.dismiss())
+                        .show();
+                return;
+            }
+
+            CharSequence[] items = new CharSequence[cityArrayList.size()];
+            for(int i = 0; i < cityArrayList.size(); ++i){
+                items[i] = cityArrayList.get(i).getName() + "   " + cityArrayList.get(i).getProvince();
+            }
+
+            new android.app.AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Remove a City")
+                    .setItems(items, (dialog, listener) -> {
+                        City cityToDelete = cityArrayList.get(listener);
+                        DocumentReference docRef = citiesRef.document(cityToDelete.getName());
+                        docRef.delete();
+                        cityArrayList.remove(listener);
+                        cityArrayAdapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("Cancel", (dialog, listener) -> dialog.dismiss())
+                    .show();
+        });
+
+        cityListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            City city = cityArrayAdapter.getItem(i);
+            CityDialogFragment cityDialogFragment = CityDialogFragment.newInstance(city);
+            cityDialogFragment.show(getSupportFragmentManager(),"City Details");
+        });
+
+    }
+
+    @Override
+    public void updateCity(City city, String title, String year) {
+        city.setName(title);
+        city.setProvince(year);
+        cityArrayAdapter.notifyDataSetChanged();
+
+        // Updating the database using delete + addition
+    }
+
+    @Override
+    public void addCity(City city){
+        cityArrayList.add(city);
+        cityArrayAdapter.notifyDataSetChanged();
+
+        DocumentReference docRef = citiesRef.document(city.getName());
+        docRef.set(city);
+    }
+
+    public void addDummyData(){
+        City m1 = new City("Edmonton", "AB");
+        City m2 = new City("Vancouver", "BC");
+        cityArrayList.add(m1);
+        cityArrayList.add(m2);
+        cityArrayAdapter.notifyDataSetChanged();
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# CMPUT 301 : Lab 5 - Firestore
+
+## Student Details
+
+- **Full Name:** `Yousef Moussa`
+- **CCID:** `ymoussa`
+
+## References and Resources
+
+`N/A`
+
+## Verbal Collaboration
+
+| Student Name | CCID     |
+| ------------ | -------- |
+| `N/A` | `N/A` |
